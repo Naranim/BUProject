b149ecb69bad201c15071b4119424e23d8e9d53f
Jakub Czarnowicz <j.czarnowicz@gmail.com>
2013-02-02 20:52:07 +0100

    documenting

--- slave_server/SlaveMasterProtocol.py
+++ slave_server/SlaveMasterProtocol.py
@@ -8,6 +8,9 @@ import slave_server.SlaveClientProtocol
 from _thread import *
 
 def generateFileList() :
+    """
+    Zwraca string zawierajacy pelna informacje o przechowywanych plikach
+    """
     filesPath = FILES_PATH
     ret = []
     for login in os.listdir(filesPath) :
@@ -17,8 +20,9 @@ def generateFileList() :
 
 def connectToMaster() :
     """
-
-        """
+    Metoda do nawiazania pierwszego polaczanie z master serwerem w razie awarii.
+    Pozwala na wygenerowanie raportu o zawartych plikach i przeslaniu go na serwer glowny
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(5)
     connected = False
@@ -36,33 +40,53 @@ def connectToMaster() :
         connected = True
         s.send(str("REPORT\nName:" + SlaveName + "\n" + generateFileList() + ">>>").encode())
         answer = s.recv(100000)
-        #TODO
-        # DELETE FILES, UNKNOWN
         answer = answer.decode().split("\n")
         print(answer)
 
 
-def reportDownloadComplete(file, md5):
+def reportDownloadComplete(file, user, modified, slave_id):
+    """
+    raport dla master serwera o zakonczeniu pobierania
+    pliku file
+    od uzytkownika user
+    zmodyfikowanego modified
+    """
     try :
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((MasterAddress, MasterPort))
-        s.send("DOWNLOAD_COMPLETE\n" + file + "\n" + md5)
+        massage = "COMPLETE_DOWNLOAD\nfile:" + file + "\nuser" + user + "\nmodified" + modified + "\nslave:" + slave_id
+        s.send(massage.encode())
     finally:
         s.close()
 
-def reportUploadComplete(file, md5):
-    try :
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((MasterAddress, MasterPort))
-        s.send("UPLOAD_COMPLETE\n" + file + "\n" + md5)
-    finally:
-        s.close()
+#def reportUploadComplete(file, user, modified):
+#    """
+#    raport dla master serwera o zakonczeniu wysylania
+#    pliku file
+#    od uzytkownika user
+#    zmodyfikowanego modified
+#    """
+
+#    try :
+#        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#        s.connect((MasterAddress, MasterPort))
+#        s.send("UPLOAD_COMPLETE\n" + file + "\n" + md5)
+#    finally:
+#        s.close()
 
 def getFile(s, massage):
+    """
+    Metoda do przygotowania pobierania pliku na slava
+    Slave przechodzi w stan oczekiwania na klienta (TICKET_TIMEOUT sekund) oraz daje masterowi
+    dane potrzebne klientowi do nawiazania polaczenia
+
+    Argument to socket i zapytanie od master serwera
+    """
     try:
         fileName = massage[1].split(":")[1]
         user = massage[2].split(":")[1]
         ticket = massage[3].split(":")[1]
+        slave_id = massage[4].split(":")[1]
         sock = socket.socket()
         sock.bind(("", 0))
         port = sock.getsockname()[1]
@@ -75,17 +99,25 @@ def getFile(s, massage):
     except socket.error :
         print("Error getting file: " + fileName + " from " + user)
 
-    res = slave_server.SlaveClientProtocol.getFile(s, fileName, user, ticket, fallowersAddresses)
+    res = slave_server.SlaveClientProtocol.getFile(s, fileName, user, ticket, fallowersAddresses, slave_id)
 
-    reportDownloadComplete(res, fileName, md5, user)
+    reportDownloadComplete(res, fileName, user)
 
 
 def giveFile(s, massage):
+    """
+    Metoda do przygotowania pobierania pliku przez klienta
+    Slave przechodzi w stan oczekiwania na klienta (TICKET_TIMEOUT sekund) oraz daje masterowi
+    dane potrzebne klientowi do nawiazania polaczenia
+
+    Argument to socket i zapytanie od master serwera
+    """
+
     try:
         fileName = massage[1].split(":")[1]
         user = massage[2].split(":")[1]
         ticket = massage[3].split(":")[1]
-        md5 = massage[4].split(":")[1]
+        slave_id = massage[4].split(":")[1]
         sock = socket.socket()
         sock.bind(("", 0))
         port = sock.getsockname()[1]
@@ -101,27 +133,15 @@ def giveFile(s, massage):
 
     res = slave_server.SlaveClientProtocol.giveFile(s, fileName, user, ticket)
 
-    reportDownloadComplete(res, fileName, md5, user)
+    reportDownloadComplete(res, fileName, user)
 
 
+def deleteFile(massage):
+    """
+    metoda do usuwania konkretnego pliku z serwera
 
-def moveFileFromTmp(massage) :
-    fileName = massage[1].split(":")[1]
-    user = massage[2].split(":")[1]
-    try :
-        shutil.move(TMP_PATH + user + "/" + fileName, FILES_PATH + user)
-    except IOError:
-        print("Error moving " + fileName)
-
-def moveFileToTmp(massage) :
-    fileName = massage[1].split(":")[1]
-    user = massage[2].split(":")[1]
-    try :
-        shutil.move(FILES_PATH + user + "/" + fileName, TMP_PATH + user)
-    except IOError:
-        print("Error moving " + fileName)
-
-def deleteFile(s, massage):
+    Argument to zapytanie od master serwera
+    """
     fileName = massage[1].split(":")[1]
     user = massage[2].split(":")[1]
     try:
@@ -129,16 +149,13 @@ def deleteFile(s, massage):
     except IOError:
         print("Error deleting " + fileName)
 
-def deleteFileInTmp(massage):
-    fileName = massage[1].split(":")[1]
-    user = massage[2].split(":")[1]
-    try:
-        os.remove(TMP_PATH + user + "/" + fileName)
-    except IOError:
-        print("Error deleting " + fileName)
 
-def reportState(s, massage):
-    s.close()
+def reportState(massage):
+    """
+    Metoda do raportowania swojego stanu masterowi.Generuje liste przechowywanych plikow
+
+    Argument to zapytanie od master serwera
+    """
     connectToMaster()
 
 
